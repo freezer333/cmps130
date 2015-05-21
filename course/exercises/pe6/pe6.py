b9bba39139ecdb6bf72af90fe13a61040b835e13
Scott Frees <scott.frees@gmail.com>
2015-05-21 17:26:09 -0400

    programming exercise 6

--- course/exercises/pe6/pe6.py
+++ course/exercises/pe6/pe6.py
@@ -1,17 +1,12 @@
 ##############################################################################
-# Programming Excercise 4
+# Programming Excercise 6
 #
-# In order to qualify for a particular loan, lets assume you must 
-# earn over $60K a year or earn over $35K and have worked at 
-# your current job for over 5 years.
-# Write program that asks for user's salary and years at current job.
-# Output whether or not they qualify.
+# Ask the user to enter 5 numbers that are divisible by 3 and 5.
+# Don’t let them stop until they’ve entered 5 of them!
 #############################################################################
 
-salary = float(input("Please enter your current salary:  $"))
-years = float(input("Please enter the number of years you've been at this job:  "))
-
-if salary >= 60000 or salary >= 35000 and years >= 5 :
-    print("Congratulations, your load has been approved!")
-else :
-    print("Sorry, we cannot give you the load...")
+count = 0
+while count < 5 :
+    val = int(input("Enter a value divisible by 3 and 5:  "))
+    if val % 3 == 0 and val % 5 == 0 :
+        count += 1
