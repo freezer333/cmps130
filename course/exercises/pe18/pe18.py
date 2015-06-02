11e4938e83548692ea8c59e5384625e013aad38c
Scott Frees <scott.frees@gmail.com>
2015-06-02 11:28:40 -0400

    typo fixes

--- course/exercises/pe18/pe18.py
+++ course/exercises/pe18/pe18.py
@@ -12,7 +12,7 @@ def findRoots(a, b, c):
     if d < 0:
         return ()
     elif d == 0:
-        return (-b / (2 * a))
+        return (-b / (2 * a),)
     else:
         x1 = (-b + math.sqrt(d))/(2 * a)
         x2 = (-b - math.sqrt(d))/(2 * a)
